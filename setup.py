5e8d08709d3fbd9e12c9258c9150fe501b511be0
Haruki Isono <haruki.isono861@gmail.com>
2026-07-07 14:32:14 +0900

    chore

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ package_name = 'cibo'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='2.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name, ["pyproject.toml"]),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
         (os.path.join('share', package_name, 'rviz'),   glob('rviz/*.rviz')),
         (os.path.join('share', package_name), glob('urdf/*')),
