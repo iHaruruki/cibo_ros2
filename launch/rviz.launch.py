d30c3b01c88e87f8393370bb1cf95398b29f5a92
Haruki Isono <haruki.isono861@gmail.com>
2026-08-10 19:36:56 +0900

    feat

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -1,24 +1,76 @@
-# Copyright (c) 2025 Haruki Isono
-# This software is released under the MIT License, see LICENSE.
-
-from ament_index_python import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
 import os
 
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
 def generate_launch_description():
-    
-    rviz_config_dir = os.path.join(
-        get_package_share_directory('cibo'),
-        'rviz', 'cibo_camera.rviz'
+
+    namespace = LaunchConfiguration("namespace")
+    namespace_cmd = DeclareLaunchArgument(
+        "namespace",
+        default_value="cibo",
+        description="Namespace for the nodes",
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    package_share = get_package_share_directory("cibo_ros2")
+
+    urdf = os.path.join(
+        package_share,
+        "urdf",
+        "cibo.urdf",
+    )
+
+    rviz_config = os.path.join(
+        package_share,
+        "rviz",
+        "cibo_3d.rviz",
+    )
+
+    with open(urdf, "r") as f:
+        robot_desc = f.read()
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        namespace=namespace,
+        output="screen",
+        parameters=[
+            {
+                "use_sim_time": use_sim_time,
+                "robot_description": robot_desc,
+            }
+        ],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        namespace=namespace,
+        output="screen",
+        arguments=["-d", rviz_config],
+        parameters=[
+            {
+                "use_sim_time": use_sim_time,
+            }
+        ],
     )
 
     return LaunchDescription([
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            output='screen'
-        ),
+        namespace_cmd,
+        use_sim_time_cmd,
+        robot_state_publisher,
+        rviz_node,
     ])
